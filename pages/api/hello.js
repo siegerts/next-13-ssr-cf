513430377d9c62712597a9533b37e64ddaf0524d
siegerts <stephen.siegert@gmail.com>
2023-03-10 14:17:28 -0500

    adjust api route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === "POST") {
+    res.status(200).json({ status: "ok", ...req.body });
+  } else if (req.method === "GET") {
+    // Process a GET request
+    res.status(200).json({ name: "John Doe" });
+  } else if (req.method === "PUT") {
+    res.status(200).json({ status: "ok", ...req.body });
+  } else if (req.method === "DELETE") {
+    res.status(200).json({ status: "ok" });
+  } else {
+    res.setHeader("Allow", ["POST", "GET", "PUT", "DELETE"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
