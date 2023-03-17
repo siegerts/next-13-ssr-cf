407ddb9a1263c287da4dff9376045fd5e328bfb3
siegerts <stephen.siegert@gmail.com>
2023-03-17 09:43:49 -0400

    add new api route

---
+++ pages/api/name.js
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.setHeader("Cache-Control", "s-maxage=0");
+  res.setHeader("Content-Type", "application/json");
+
+  console.log(req.method);
+
+  if (req.method === "POST") {
+    res.status(200).json({ status: "ok", ...req.body });
+  } else if (req.method === "GET") {
+    // Process a GET request
+    res.status(200).json({ name: "John Doe" });
+  } else if (req.method === "PUT") {
+    res.status(200).json({ status: "ok", ...req.body });
+  } else if (req.method === "DELETE") {
+    res.status(200).json({ status: "ok" });
+  } else {
+    res.setHeader("Allow", ["POST", "GET", "PUT", "DELETE"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
