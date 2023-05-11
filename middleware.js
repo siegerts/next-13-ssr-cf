930045592c0f3e9fb677f66316b6b0d7d98d2453
siegerts <stephen.siegert@gmail.com>
2023-05-11 16:55:01 -0400

    json.parse headers...

--- middleware.js
+++ middleware.js
@@ -6,9 +6,11 @@ export default async function middleware(req, res) {
 
   if (url.pathname === "/") {
     // mapping of CloudFront-Viewer-Country to language code
-    const countryCode = await req.headers["cloudfront-viewer-country"];
+    const countryCode = await JSON.parse(
+      req.headers["cloudfront-viewer-country"]
+    );
 
-    console.log(await req.headers["cloudfront-viewer-country"]);
+    console.log(await JSON.parse(req.headers["cloudfront-viewer-country"]));
 
     console.log("country code: ", countryCode);
 
